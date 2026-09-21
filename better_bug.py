b8e8dbde0f3480c855648fd68902386118023b4c
Lin Bing Yuan <298345430+8penny1po@users.noreply.github.com>
2026-09-22 01:49:58 +0800

    fix bug

--- better_bug.py
+++ better_bug.py
@@ -8,25 +8,9 @@ def get_data(d):
     cd=os.path.dirname(os.path.abspath(__file__))
     dbp=os.path.join(cd,'stock.db')
     conn=sqlite3.connect(dbp)
-    cookies = {
-        '_ga': 'GA1.1.79233902.1782883600',
-        '_ga_J2HVMN6FVP': 'GS2.1.s1783347540$o7$g0$t1783347540$j60$l0$h0',
-    }
 
     headers = {
-        'accept': 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'priority': 'u=1, i',
-        'referer': 'https://www.twse.com.tw/zh/trading/historical/mi-index.html',
-        'sec-ch-ua': '"Google Chrome";v="153", "Not_A Brand";v="8", "Chromium";v="153"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36',
-        'x-requested-with': 'XMLHttpRequest',
-        # 'cookie': '_ga=GA1.1.79233902.1782883600; _ga_J2HVMN6FVP=GS2.1.s1783347540$o7$g0$t1783347540$j60$l0$h0',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36'
     }
 
     params = {
@@ -36,7 +20,7 @@ def get_data(d):
         '_': '1789152551771',
     }
 
-    response = requests.get('https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX', params=params, cookies=cookies, headers=headers).json()
+    response = requests.get('https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX', params=params, headers=headers).json()
     print(type(response))
     print(response.keys())
     '''
